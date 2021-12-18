cdde5c4d8293e8346a3dc555fd2e04998f2eb327
Pann <pansy.miss@gmail.com>
2021-12-18 13:38:49 +0100

    Added Footer with Github URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Pann and{" "}
+        <a
+          href="https://github.com/pansypanneiphyu/weather-app-with-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
