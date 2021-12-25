c91984b2413b3494db2063747eea324ff5098e4f
Pann <pansy.miss@gmail.com>
2021-12-25 16:18:04 +0100

    installed asxios, added API

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
         <footer>
           This project is created by Pann and{" "}
           <a href="https://github.com/pansypanneiphyu/weather-app-with-react">
--- src/Weather.js
+++ src/Weather.js
@@ -1,47 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="search-form"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>New York</h1>
-      <ul>
-        <li>Wednesday 07:00</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png" />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ show: false });
+  function handleResponse(response) {
+    setWeatherData({
+      show: true,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].decription,
+      date: "Wednesday 07:00",
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      city: response.data.name,
+    });
+  }
 
-          <span className="temperature">6</span>
-          <span className="unit">ºC</span>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 76%</li>
-            <li>Humidity: 74%</li>
-            <li>Wind: 10km/h</li>
-          </ul>
+  if (weatherData.show) {
+    return (
+      <div className="weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="search-form"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.decription}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">ºC</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "508ac6130b578f03ce3e0dfbfbfa0cdc";
+    let city = "New York";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading..";
+  }
 }
