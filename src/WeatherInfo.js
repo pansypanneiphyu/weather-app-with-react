a458e1ac682b929e259223d47aa9883d7e049ede
Pann <pansy.miss@gmail.com>
2021-12-27 15:13:26 +0100

    Added custom weather icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,11 +14,9 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="clearfix">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left"
-            />
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={50} />
+            </div>
 
             <div className="float-left">
               <span className="temperature">
