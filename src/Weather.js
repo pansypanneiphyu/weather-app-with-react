84bc3a2e245aa5d853a301effec18b2379ffeafe
Pann <pansy.miss@gmail.com>
2022-01-02 16:06:18 +0100

    updated apiUrl

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "508ac6130b578f03ce3e0dfbfbfa0cdc";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
