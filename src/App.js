09159011fe36a4de9379c472e1be2f8a6210fda5
Pann <pansy.miss@gmail.com>
2022-01-02 16:55:20 +0100

    Added WeatherForecast.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="New York" />
         <footer>
-          This project is created by Pann and{" "}
+          This project is coded by Pann and{" "}
           <a href="https://github.com/pansypanneiphyu/weather-app-with-react">
-            open-sourced on GitHub
+            open-sourced on GitHub{" "}
           </a>
+          and hosted on{" "}
+          <a href="https://cocky-albattani-ed7758.netlify.app">Netlify</a>
         </footer>
       </div>
     </div>
