51233aa5da39db02d8c7857bc1edbe790c60751f
Pann <pansy.miss@gmail.com>
2022-01-03 14:04:03 +0100

    Added loop

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index < 5) {
               return (
                 <div className="col" key={index}>
-                  <WeatherForecastDay data={forecast[0]} />
+                  <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
             }
